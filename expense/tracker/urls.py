96948743b24d0b634a14cb263e02f846671ba924
ashish <ashishbmore1@gmail.com>
2025-01-07 18:02:41 +0530

    Implemented Authentication

--- expense/tracker/urls.py
+++ expense/tracker/urls.py
@@ -4,5 +4,8 @@ from tracker.views import *
 
 urlpatterns = [
     path('',index),
+    path('register/',rigistration,name='register'),
+    path('login/',login_user,name='login'),
+    path('logout/',logot_user,name='logout'),
     path('delete-transaction/<uid>',deleteTransaction,name="deleteTransaction")
 ]
--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from tracker.models import * 
-from django.db.models import Sum
+from django.db.models import Sum,Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
 
 def index(request):
     if request.method=='POST':
@@ -36,3 +39,52 @@ def deleteTransaction(request,uid):
     Transaction.objects.get(uuid=uid).delete()
     return  redirect("/")
      
+def rigistration(request):
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        user_name=request.POST['user_name']
+        email=request.POST['email']
+        password=request.POST['password']
+        if User.objects.filter(Q(username=user_name)| Q(email=email)).exists():
+            messages.error(request, "Username already exists")
+            return redirect('/register')
+        elif Q(first_name=="")| Q(last_name=="") | Q(user_name=="") | Q(email=="") |Q(password==""):
+            messages.error(request, "Fields cannot be blank")
+            return redirect('/register')
+        else:
+            User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=user_name,
+                email=email,
+                password=password,
+            )
+            messages.success(request, "Registration successfull")
+            return redirect('/register')
+    else:
+        return render(request,'registration.html')
+
+def login_user(request):
+    if request.method=='POST':
+        user_name=request.POST['user_name']
+        password=request.POST['password']
+
+        if not user_name or not password:
+            messages.error(request,'Fields cannot be empty')
+            return redirect('/login')
+        else:
+            user = authenticate(username=user_name, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+             messages.error(request,'login failed')
+             return redirect('/login')
+    else:
+        return render(request,'login.html')
+    
+def logot_user(request):
+    logout(request)
+    messages.error(request,"Logout successful")
+    return redirect('/login')
